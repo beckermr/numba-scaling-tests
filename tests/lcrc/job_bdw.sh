ae58b3c1ec4aea57d8b6ef09598ec8ff8638cc26
Matthew R Becker <becker.mr@gmail.com>
2019-05-29 13:32:58 -0500

    actually turn off numba

--- tests/lcrc/job_bdw.sh
+++ tests/lcrc/job_bdw.sh
@@ -24,12 +24,12 @@ done
 
 # turn off numba
 
-export NUMBA_DISABLE_JIT=0
+export NUMBA_DISABLE_JIT=1
 
-srun --export=NUMBA_DISABLE_JIT,ALL -n 1 python -m cProfile -s cumtime ../numba-test-script 1 >& data1_bdw_nn.txt
-srun --export=NUMBA_DISABLE_JIT,ALL -n 2 python -m cProfile -s cumtime ../numba-test-script 1 >& data2_bdw_nn.txt
+srun -n 1 python -m cProfile -s cumtime ../numba-test-script 1 >& data1_bdw_nn.txt
+srun  -n 2 python -m cProfile -s cumtime ../numba-test-script 1 >& data2_bdw_nn.txt
 
 for nc in 1 2 4 8 16 32
 do
-    srun --export=NUMBA_DISABLE_JIT,ALL -n ${nc} python -m cProfile -s cumtime ../numba-test-script 1 >& data${nc}_bdw_nn.txt
+    srun -n ${nc} python -m cProfile -s cumtime ../numba-test-script 1 >& data${nc}_bdw_nn.txt
 done
