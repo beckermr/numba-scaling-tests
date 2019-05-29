10063375b36fb490bb53a31ba2eb34dc9d41abb2
Matthew R Becker <becker.mr@gmail.com>
2019-05-29 13:47:09 -0500

    add knl runs

--- tests/munge_data.py
+++ tests/munge_data.py
@@ -1,4 +1,5 @@
 import os
+import glob
 import sys
 import subprocess
 
@@ -6,63 +7,78 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-fnames = sys.argv[1:]
-print('number of files:', len(fnames), flush=True)
+def _extract_data(tail):
+    fnames = glob.glob('data*%s.txt' % tail)
+    print('number of files:', len(fnames), flush=True)
 
-# extract number of ranks
-ranks = [
-    int(os.path.basename(fname).split('_')[0].replace('data', ''))
-    for fname in fnames]
+    # extract number of ranks
+    ranks = [
+        int(os.path.basename(fname).split('_')[0].replace('data', ''))
+        for fname in fnames]
 
-# now extract max times
-run_times = []
-std_run_times = []
-compile_times = []
-std_compile_times = []
-for fname, n_ranks in zip(fnames, ranks):
-    print(fname)
+    # now extract max times
+    run_times = []
+    std_run_times = []
+    compile_times = []
+    std_compile_times = []
+    for fname, n_ranks in zip(fnames, ranks):
+        # munge the run time
+        output = subprocess.check_output(
+                'grep -E "\\d{4}:" %s' % fname,
+                shell=True).decode('utf-8')
+        data = []
+        for line in output.split('\n'):
+            line = line.strip()
+            if line == '':
+                continue
+            data.append(float(line.split(':')[1].strip()))
+        run_times.append(np.max(data))
+        std_run_times.append(np.std(data))
 
-    # munge the run time
-    output = subprocess.check_output(
-            'grep -E "\\d{4}:" %s' % fname,
-            shell=True).decode('utf-8')
-    data = []
-    for line in output.split('\n'):
-        line = line.strip()
-        if line == '':
-            continue
-        data.append(float(line.split(':')[1].strip()))
-    run_times.append(np.max(data))
-    std_run_times.append(np.std(data))
+        # munge the compile time
+        try:
+            output = subprocess.check_output(
+                    'grep "_acquire_compile_lock" %s' % fname,
+                    shell=True).decode('utf-8')
+            data = []
+            for line in output.split('\n'):
+                line = line.strip()
+                if line == '':
+                    continue
+                data.append(float(line.split()[3]))
+            compile_times.append(np.max(data))
+            std_compile_times.append(np.std(data))
+        except Exception:
+            compile_times.append(np.nan)
+            std_compile_times.append(np.nan)
 
-    # munge the compile time
-    output = subprocess.check_output(
-            'grep "_acquire_compile_lock" %s' % fname,
-            shell=True).decode('utf-8')
-    data = []
-    for line in output.split('\n'):
-        line = line.strip()
-        if line == '':
-            continue
-        data.append(float(line.split()[3]))
-    compile_times.append(np.max(data))
-    std_compile_times.append(np.std(data))
+    print('rank     run run_sd    cmp cmp_sd')
+    inds = np.argsort(ranks)
+    for ind in inds:
+        loss = run_times[ind] / run_times[inds[0]]
+        print('% 5d % 6.2f % 6.2f % 6.2f % 6.2f % 6.2f' % (
+            ranks[ind], run_times[ind], std_run_times[ind],
+            compile_times[ind], std_compile_times[ind],
+            loss))
 
-print('rank     run run_sd    cmp cmp_sd')
-inds = np.argsort(ranks)
-for ind in inds:
-    loss = run_times[ind] / run_times[inds[0]]
-    print('% 5d % 6.2f % 6.2f % 6.2f % 6.2f % 6.2f' % (
-        ranks[ind], run_times[ind], std_run_times[ind],
-        compile_times[ind], std_compile_times[ind],
-        loss))
+    return (
+        np.array(ranks)[inds],
+        np.array(run_times)[inds],
+        np.array(std_run_times)[inds],
+        np.array(compile_times)[inds],
+        np.array(std_compile_times)[inds])
+
+
+tail = sys.argv[1]
+ranks, r, _, c, _ = _extract_data(tail)
+ranks_nn, r_nn, _, c_nn, _ = _extract_data(tail+'_nn')
+
 
 # make a plot
 plt.figure()
-plt.loglog(
-    np.array(ranks)[inds], np.array(run_times)[inds], label='total run')
-plt.loglog(
-    np.array(ranks)[inds], np.array(compile_times)[inds], label='compile')
+plt.plot(ranks, r, label='run - numba')
+plt.plot(ranks, c, label='compile - numba')
+plt.plot(ranks_nn, r_nn, label='run - no numba')
 plt.legend()
 plt.xlabel('# of ranks')
 plt.ylabel('time [seconds]')
